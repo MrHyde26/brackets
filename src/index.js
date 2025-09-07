3b99d29ba7210feac41333c2456deca8c820043c
Artem <drta26@gmail.com>
2025-09-07 13:31:08 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const bracketsPairs = bracketsConfig.map((arr) => arr.join(''));
+  let currentStr = str;
+  let changed = true;
+
+  while (changed && currentStr.length > 0) {
+    changed = false;
+
+    const prevLength = currentStr.length;
+
+    for (let i = 0; i < bracketsPairs.length; i += 1) {
+      currentStr = currentStr.replace(bracketsPairs[i], '');
+    }
+
+    if (currentStr.length < prevLength) {
+      changed = true;
+    }
+  }
+
+  return currentStr.length === 0;
 };
